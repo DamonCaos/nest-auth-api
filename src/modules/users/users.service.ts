2e6f96ae3d0b0c626361a189398193593fc121a3
Javier Rodriguez <franciscoromera0816@gmail.com>
2026-03-06 13:15:09 +0100

    feat: prepare auth infrastructure with users service and JWT dependencies

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+
+  findByEmail(email: string) {
+    return this.users.find((user) => user.email === email);
+  }
+
+  create(userData: User) {
+    this.users.push(userData);
+    return userData;
+  }
+
+  findAll() {
+    return this.users.map(({ password, ...user }) => user);
+  }
+}
