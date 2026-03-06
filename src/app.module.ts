3b52134ad36945bb90b5dabf4f793e0310f361c5
Javier Rodriguez <franciscoromera0816@gmail.com>
2026-03-06 12:19:27 +0100

    feat: scaffold health, users and auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HealthModule } from './modules/health/health.module';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    HealthModule,
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
